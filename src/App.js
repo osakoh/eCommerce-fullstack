38358f8d94aa43a08cc795734e67327db8008ef4
osakoh <macmive@gmail.com>
2021-07-27 14:21:28 +0100

    changed a tag to linkcontainer from react router bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Pages/Home";
+import Product from "./components/Pages/ProductPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route exact path='/' component={Home} />
+          <Route exact path='/product/:_id' component={Product} />
         </Container>
       </main>
       <Footer />
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,33 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { IoCartOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Ecommerce</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Ecommerce</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='mr-auto'>
-              <Nav.Link href='/cart'>
-                <IoCartOutline style={{ fontSize: "1.2rem" }} />
-                &nbsp;Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaRegUser style={{ fontSize: "1.2rem" }} />
-                &nbsp;Login
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <IoCartOutline style={{ fontSize: "1.2rem" }} />
+                  &nbsp;Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaRegUser style={{ fontSize: "1.2rem" }} />
+                  &nbsp;Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
