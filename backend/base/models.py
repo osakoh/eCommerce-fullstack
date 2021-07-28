5854bd5074233f4d7d64970fbd9f1dbe68cd94e7
osakoh <macmive@gmail.com>
2021-07-28 13:50:21 +0100

    created Order & OrderItem models

--- backend/base/models.py
+++ backend/base/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 
 
 class Product(models.Model):
+    """
+    Many Products to one User/One User to many Products
+    """
+
     # models.SET_NULL: doesn't delete product if user gets deleted | null=True: allows a product to be created without a user
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -25,6 +29,11 @@ class Product(models.Model):
 
 
 class Review(models.Model):
+    """
+    Many reviews to one Product
+    Many reviews to one User
+    """
+
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
@@ -35,3 +44,48 @@ class Review(models.Model):
 
     def __str__(self):
         return str(self.rating)
+
+
+class Order(models.Model):
+    """
+    Many Orders to one User
+    """
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    taxPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    totalPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True
+    )
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.BooleanField(default=False)
+    deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    """
+    Many OrderItems to one Product
+    Many OrderItems to one Order
+    """
+
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
