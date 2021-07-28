daad3521a912b2525a6e2745dce539e1575f8855
osakoh <macmive@gmail.com>
2021-07-28 13:38:43 +0100

    created & migrated the Product model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    # models.SET_NULL: doesn't delete product if user gets deleted | null=True: allows a product to be created without a user
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True, default="/placeholder.png")
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    # decimal_places=2: two values after the decimal
+    rating = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    # auto_now_add=True: saves the date the first time a product is created
+    createdAt = models.DateTimeField(auto_now_add=True)
+    # overrides the default id field and stop users from editing it
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
