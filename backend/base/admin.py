034be53e9f4ade8443f8db2c7808b973c476de79
osakoh <macmive@gmail.com>
2021-07-28 13:53:55 +0100

    added Order, OrderItem, & ShippingAddress models to the admin

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -3,6 +3,6 @@ from .models import *
 
 admin.site.register(Product)
 admin.site.register(Review)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
-# admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(ShippingAddress)
