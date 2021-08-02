310054b9aca5f0b8935d0f078495fffc86a950ce
osakoh <macmive@gmail.com>
2021-08-02 15:50:47 +0100

    added productdetailsreducer to store

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -3,9 +3,13 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
-export const productListReducers = (state = { products: [] }, action) => {
+// retreives list of products
+export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     // loading the product list
     case PRODUCT_LIST_REQUEST:
@@ -29,6 +33,26 @@ export const productListReducers = (state = { products: [] }, action) => {
       };
 
     // returns the initial state
+    default:
+      return state;
+  }
+};
+
+// retreives a product based on the id of the product
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+
     default:
       return state;
   }
