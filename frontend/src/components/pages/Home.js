fb50ff91b92fc0709c9540ed402617038149276b
osakoh <macmive@gmail.com>
2021-08-02 15:41:42 +0100

    selected state from store

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import ProductItem from "../common/ProductItem";
-import axios from "axios";
+import { listProducts } from "../../actions/productActions";
+import Loader from "../common/Loader";
+import Message from "../common/Message";
 
 const Home = () => {
-  // products state
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  //select states from productList
+  const productList = useSelector((state) => state.productList);
+  // destructure states from productList: productListReducers
+  const { loading, error, products } = productList;
 
   // api call
-  useEffect(
-    () => {
-      const fetchProducts = async () => {
-        try {
-          const { data } = await axios.get("/api/products/");
-          // setproducts
-          setProducts(data);
-        } catch (error) {
-          console.log(error, "from Home JS");
-        }
-      };
+  useEffect(() => {
+    // dispatch listProducts
+    dispatch(listProducts());
+  }, [dispatch]);
 
-      // call fetchProducts method
-      fetchProducts();
-    },
-
-    // eslint-disable-next-line
-    []
-  );
-
-  return (
-    <div>
-      <h1>Latest Products</h1>
+  // variable to hold the content depending on loading, error or products
+  let homeContent;
+  if (loading) {
+    homeContent = <Loader />;
+  } else if (error) {
+    homeContent = (
+      <Message variant='danger' heading='Error Loading!'>
+        {error}
+      </Message>
+    );
+  } else {
+    homeContent = (
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={7} lg={4} xl={3}>
@@ -38,6 +38,13 @@ const Home = () => {
           </Col>
         ))}
       </Row>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      {homeContent}
     </div>
   );
 };
